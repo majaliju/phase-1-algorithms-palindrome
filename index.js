58c4ac3635f3dc22e1410f0f5b6b70470379e349
majaliju <majalijunyc@gmail.com>
2022-03-27 19:47:55 -0400

    updated with better approach

--- index.js
+++ index.js
@@ -1,27 +1,47 @@
-function isPalindrome(word) {
-
-  let word = "a"
-  console.log("word 'a': ", word)
-
-  wordArray = Array.from(word)
-  console.log("wordArray: ", wordArray)
-
-  // establish array2, the backwards array
-  const array2 = []
-  for (i = (wordArray.length-1); i >= 0; i--){
-    array2.push(wordArray[i]);
-  }
-  console.log("array2: ", array2)
-
-  // this code block down here isn't perfect 
-  // this one has errors
-  if (wordArray === array2){
-    return true
-  } 
-  else {
-    return false
-  }
+function reverse(word) {
+  const wordArray = word.split("")
+  const reversedWordArray = wordArray.reverse()
+  const reversedWord = reversedWordArray.join("")
+  return reversedWord
 }
+
+function isPalindrome(word){
+  const wordInReverse = reverse(word)
+
+  if (word === wordInReverse) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+
+
+// // my initial approach
+// function isPalindrome(word) {
+
+//   let word = "a"
+//   console.log("word 'a': ", word)
+
+//   wordArray = Array.from(word)
+//   console.log("wordArray: ", wordArray)
+
+//   // establish array2, the backwards array
+//   const array2 = []
+//   for (i = (wordArray.length-1); i >= 0; i--){
+//     array2.push(wordArray[i]);
+//   }
+//   console.log("array2: ", array2)
+
+//   // this code block down here isn't perfect 
+//   // this one has errors
+//   if (wordArray === array2){
+//     return true
+//   } 
+//   else {
+//     return false
+//   }
+// }
 
 
 /* 
